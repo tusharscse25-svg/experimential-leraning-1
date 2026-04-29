8e58a38d097527065ce73bf00c2f9f9a3f5c0ef1
tusharscse25-svg <tushars.cse25@sbjit.edu.in>
2026-04-29 19:30:32 +0530

    Add page navigation and feedback form handling

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
+function showPage(pageId) {
+    let pages = document.querySelectorAll(".page");
+    pages.forEach(p => p.classList.remove("active"));
+    document.getElementById(pageId).classList.add("active");
+}
 
+let rating = document.getElementById("rating");
+let ratingValue = document.getElementById("ratingValue");
+
+rating.oninput = function () {
+    ratingValue.innerText = this.value;
+};
+
+document.getElementById("feedbackForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    showPage("thankyou");
+});
